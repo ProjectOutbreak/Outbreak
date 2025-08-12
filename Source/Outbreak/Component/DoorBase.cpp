a28a27148fd395756666f811dba2c47c400ec16d
KariV <156992938+KariV-824@users.noreply.github.com>
2025-08-12 12:03:13 +0900

    feat : Door Component Add (Partial)

---
+++ Source/Outbreak/Component/DoorBase.cpp
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DoorBase.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ADoorBase::ADoorBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	DoorFrame = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorFrameMesh"));
+	DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
+	DoorTimelineComp = CreateDefaultSubobject<UTimelineComponent>(TEXT("DoorTimelineComp"));
+	DoorProxVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("DoorProximityVolume"));
+
+	DoorFrame->SetupAttachment(RootComponent);
+	DoorMesh->AttachToComponent(DoorFrame, FAttachmentTransformRules::KeepRelativeTransform);
+	DoorProxVolume->AttachToComponent(DoorFrame, FAttachmentTransformRules::KeepRelativeTransform);
+}
+
+// Called when the game starts or when spawned
+void ADoorBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UpdateFunctionFloat.BindDynamic(this, &ADoorBase::UpdateTimelineComp);
+	if (DoorTimelineFloatCurve)
+	{
+		DoorTimelineComp->AddInterpFloat(DoorTimelineFloatCurve, UpdateFunctionFloat);
+	}
+	DoorProxVolume->OnComponentBeginOverlap.AddDynamic(this, &ADoorBase::OnOverlapBegin);
+	DoorProxVolume->OnComponentEndOverlap.AddDynamic(this, &ADoorBase::OnOverlapEnd);
+
+}
+
+// Called every frame
+void ADoorBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ADoorBase::DoorOpen()
+{
+	
+}
+
+void ADoorBase::DoorClose()
+{
+	
+}
+
+bool ADoorBase::IsOpen() const
+{
+	return CurrentStatus == EDoorStatus::Open;
+}
+
+void ADoorBase::UpdateTimelineComp(float Output)
+{
+	FRotator DoorNewRotation = FRotator(0.0f,Output,0.0f);
+	DoorMesh -> SetRelativeRotation(DoorNewRotation);
+}
+
+void ADoorBase::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	DoorTimelineComp->Play();
+}
+ 
+void ADoorBase::OnOverlapEnd(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	DoorTimelineComp->Reverse();
+}
+
+
+
---
+++ Source/Outbreak/Component/DoorBase.h
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/TimelineComponent.h"
+#include "DoorBase.generated.h"
+
+UENUM(BlueprintType)
+enum class EDoorStatus : uint8
+{
+	Open,
+	Close
+};
+
+UCLASS()
+class OUTBREAK_API ADoorBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADoorBase();
+	
+	void DoorOpen();
+	
+	void DoorClose();
+	
+	bool IsOpen() const;
+
+	UPROPERTY(EditAnywhere)
+	UCurveFloat* DoorTimelineFloatCurve;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* DoorMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* DoorFrame;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door Settings")
+	bool bIsCanInteract = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door Settings")
+	bool bIsDestroyable = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door Settings", meta = (EditCondition = "bIsDestroyable"))
+	int HP = 100;
+	
+	//UPROPERTY(Replicated, BlueprintReadOnly, Category = "Door State")
+	EDoorStatus CurrentStatus = EDoorStatus::Close;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UTimelineComponent* DoorTimelineComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UBoxComponent* DoorProxVolume;
+	
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+private:
+	FOnTimelineFloat UpdateFunctionFloat;
+
+	UFUNCTION()
+	void UpdateTimelineComp(float Output);
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
