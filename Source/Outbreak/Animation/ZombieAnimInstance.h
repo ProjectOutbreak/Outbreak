428c9bba3cf193a2a8f72de2419f81aa84359f1f
JeongJongMun <whdans4005@naver.com>
2025-09-28 03:11:13 +0900

    feat : Zombie Animation 네이밍 & ABP 생성

---
+++ Source/Outbreak/Animation/ZombieAnimInstance.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ZombieAnimInstance.generated.h"
+
+UCLASS()
+class OUTBREAK_API UZombieAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+};
