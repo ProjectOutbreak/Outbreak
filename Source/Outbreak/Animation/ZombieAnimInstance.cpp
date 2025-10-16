6cb594e4952b8f0526b5e117a2ec8e25908b1dbc
JeongJongMun <whdans4005@naver.com>
2025-10-16 19:36:47 +0900

    feat : Zombie Animation Blueprint 내 변수 AnimInstance로 이동

--- Source/Outbreak/Animation/ZombieAnimInstance.cpp
+++ Source/Outbreak/Animation/ZombieAnimInstance.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ZombieAnimInstance.h"
+#include "KismetAnimationLibrary.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "Outbreak/Character/Zombie/CharacterZombie.h"
 
 void UZombieAnimInstance::NativeInitializeAnimation()
@@ -8,13 +10,34 @@ void UZombieAnimInstance::NativeInitializeAnimation()
 	Super::NativeInitializeAnimation();
 
 	OwnerZombie = Cast<ACharacterZombie>(TryGetPawnOwner());
+	if (!OwnerZombie)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[%s] OwnerZombie is nullptr"), CURRENT_CONTEXT);
+		return;
+	}
+	MovementComponent = OwnerZombie->GetCharacterMovement();
+	if (!MovementComponent)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[%s] MovementComponent is nullptr"), CURRENT_CONTEXT);
+		return;
+	}
 }
 
 void UZombieAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
 	Super::NativeUpdateAnimation(DeltaSeconds);
 	
-	if (!OwnerZombie) return;
+	if (!OwnerZombie || !MovementComponent)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[%s] OwnerZombie or MovementComponent is nullptr"), CURRENT_CONTEXT);
+		return;
+	}
 
 	AttackRate = OwnerZombie->GetZombieData()->AttackRate;
+	Velocity = MovementComponent->Velocity;
+	GroundSpeed = FVector(Velocity.X, Velocity.Y, 0.0f).Size();
+	Direction = UKismetAnimationLibrary::CalculateDirection(Velocity, OwnerZombie->GetActorRotation());
+	IsAttack = OwnerZombie->bIsAttacking;
+	IsScream = OwnerZombie->bIsScreaming;
+	ShouldMove = GroundSpeed > 3.0f;
 }
--- Source/Outbreak/Animation/ZombieAnimInstance.h
+++ Source/Outbreak/Animation/ZombieAnimInstance.h
@@ -6,6 +6,7 @@
 #include "Animation/AnimInstance.h"
 #include "ZombieAnimInstance.generated.h"
 
+class UCharacterMovementComponent;
 class ACharacterZombie;
 
 UCLASS()
@@ -19,8 +20,29 @@ public:
 
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly);
-	float AttackRate;
-
-	UPROPERTY(visibleAnywhere, BlueprintReadOnly);
 	TObjectPtr<ACharacterZombie> OwnerZombie;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly);
+	TObjectPtr<UCharacterMovementComponent> MovementComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly);
+	FVector Velocity;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly);
+	float AttackRate;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly);
+	float GroundSpeed;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly);
+	float Direction;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly);
+	bool ShouldMove;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly);
+	bool IsAttack;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly);
+	bool IsScream;
 };
