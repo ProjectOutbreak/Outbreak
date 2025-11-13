5be6ceeefd2332f76f9cf66a7de149826fe5953b
hankyeol <lhn030703@gmail.com>
2025-11-13 21:31:02 +0900

    feat : added AWS SDK Path

--- Source/Outbreak/Outbreak.Build.cs
+++ Source/Outbreak/Outbreak.Build.cs
@@ -1,5 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
+using System.Linq;
 using UnrealBuildTool;
 
 public class Outbreak : ModuleRules
@@ -7,20 +9,21 @@ public class Outbreak : ModuleRules
 	public Outbreak(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { 
-			"Core", 
-			"CoreUObject", 
-			"Engine", 
-			"InputCore", 
-			"MovieScene", 
-			"LevelSequence", 
-			"MovieSceneTracks", 
-			"UMG", 
-			"Slate",      
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"MovieScene",
+			"LevelSequence",
+			"MovieSceneTracks",
+			"UMG",
+			"Slate",
 			"SlateCore",
-			"EnhancedInput", 
-			"NavigationSystem", 
+			"EnhancedInput",
+			"NavigationSystem",
 			"AIModule",
 			"Sockets",
 			"Networking",
@@ -34,10 +37,49 @@ public class Outbreak : ModuleRules
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+		//Load AWS SDK 
+		bUseUnity = false;
+		LoadAWSSDK(Target);
+	}
+
+	// get full path Source/Outbreak/ThirdParty 
+	private string ThirdPartyPath
+	{
+		get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "ThirdParty/")); }
+	}
+	
+	private void LoadAWSSDK(ReadOnlyTargetRules Target)
+	{
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			string AWSSDKPath = Path.Combine(ThirdPartyPath, "AwsSDK"); 
+        
+			string SDKLibPath = Path.Combine(AWSSDKPath, "lib");
+			string SDKIncludePath = Path.Combine(AWSSDKPath, "include");
+
+			PublicIncludePaths.Add(SDKIncludePath);
+
+			string[] LibFiles = Directory.GetFiles(SDKLibPath, "*.lib", SearchOption.AllDirectories);
+			foreach (string LibFile in LibFiles)
+			{
+				PublicAdditionalLibraries.Add(LibFile);
+			}
+
+			PublicAdditionalLibraries.Add("winhttp.lib");
+			PublicAdditionalLibraries.Add("bcrypt.lib");
+			PublicAdditionalLibraries.Add("userenv.lib");     
+			PublicAdditionalLibraries.Add("version.lib");     
+			PublicAdditionalLibraries.Add("shlwapi.lib");     
+			PublicAdditionalLibraries.Add("crypt32.lib");     
+			PublicAdditionalLibraries.Add("secur32.lib");     
+			PublicAdditionalLibraries.Add("ncrypt.lib");      
+
+		}
 	}
 }
