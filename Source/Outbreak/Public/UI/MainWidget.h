d52fe7e04d336d75a809bb08fd2001ae215d9519
JeongJongMun <whdans4005@naver.com>
2025-12-31 21:30:40 +0900

    feat : Session Subsystem 구현, 기존 ui에 추가

--- Source/Outbreak/Public/UI/MainWidget.h
+++ Source/Outbreak/Public/UI/MainWidget.h
@@ -4,8 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "MainWidget.generated.h"
 
+class USessionSubsystem;
+class UEditableTextBox;
 class UButton;
 
 UCLASS()
@@ -15,16 +18,52 @@ class OUTBREAK_API UMainWidget : public UUserWidget
 
 protected:
 	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
 	
 private:
+	void SetButtonsEnabled(bool BNewIsEnabled);
+	const TCHAR* JoinSessionResultToText(EOnJoinSessionCompleteResult::Type InResult) const;
+	FString GenerateRandomLobbyCode(int32 Length);
+
+	// ~ Begin Seesion Subsystem
+	void BindSessionSubsystemCallbacks();
+	void RemoveSessionSubsystemCallbacks();
+	
+	UFUNCTION()
+	void OnCreateSession(bool bWasSuccessful);
+	void OnFindSession(const TArray<FOnlineSessionSearchResult>& SessionResults, bool bWasSuccessful);
+	void OnJoinSession(EOnJoinSessionCompleteResult::Type Result);
+	UFUNCTION()
+	void OnDestroySession(bool bWasSuccessful);
+	UFUNCTION()
+	void OnSessionError(const FString& Reason);
+	UFUNCTION()
+	void OnStartSession(bool bWasSuccessful);
+	// ~ End Session Subsystem
+	
+	// ~ Begin Button Callbacks
 	UFUNCTION()
 	void OnClickCreateGameButton();
 	UFUNCTION()
 	void OnClickJoinGameButton();
+	UFUNCTION()
+	void OnClickSinglePlayButton();
+	// ~ End Button Callbacks
 	
+	// ~ Begin UMG
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UButton> CreateGameButton;
 	
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UButton> JoinGameButton;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> SinglePlayButton;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> TB_LobbyCode;
+	// ~ End UMG
+	
+	UPROPERTY(Transient)
+	TObjectPtr<USessionSubsystem> SessionsSubsystem;
 };
