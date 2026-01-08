ac7a4adb2c6b4ecb09006a291bf2650d5d246a2b
JeongJongMun <whdans4005@naver.com>
2026-01-08 17:32:02 +0900

    feat : 좀비 공격을 트레이스가 아닌 오버랩으로 변경해서 더 정확하게 피격 판정할 수 있게 개선

---
+++ Source/Outbreak/Public/Animation/Notifies/ANS_ZombieAttack.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "ANS_ZombieAttack.generated.h"
+
+UCLASS()
+class OUTBREAK_API UANS_ZombieAttack : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+	virtual void NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, const FAnimNotifyEventReference& EventReference) override;
+};
