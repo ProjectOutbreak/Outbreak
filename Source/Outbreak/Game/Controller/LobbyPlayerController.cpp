e56498655a4ec4c5032be9146255dbe2bab85dcb
hankyeol <lhn030703@gmail.com>
2026-01-12 01:23:04 +0900

    feat : 리슨서버 & 데디서버 게임 요청 LobbyController 추가

---
+++ Source/Outbreak/Game/Controller/LobbyPlayerController.cpp
@@ -0,0 +1,15 @@
+﻿#include "LobbyPlayerController.h"
+#include "Outbreak/Game/Framework/LobbyGameMode.h"
+
+void ALobbyPlayerController::RequestStartGame()
+{
+	Server_RequestStartGame();
+}
+
+void ALobbyPlayerController::Server_RequestStartGame_Implementation()
+{
+	if (ALobbyGameMode* GM = Cast<ALobbyGameMode>(GetWorld()->GetAuthGameMode()))
+	{
+		GM->StartGame();
+	}
+}
---
+++ Source/Outbreak/Game/Controller/LobbyPlayerController.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LobbyPlayerController.generated.h"
+
+UCLASS()
+class OUTBREAK_API ALobbyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+    
+public:
+	UFUNCTION(BlueprintCallable, Category = "Lobby")
+	void RequestStartGame();
+
+protected:
+	UFUNCTION(Server, Reliable)
+	void Server_RequestStartGame();
+};
